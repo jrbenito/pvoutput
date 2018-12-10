4c5c44f5f88dd8ec7ecc4c59845c13477e35777d
Josenivaldo Benito Jr <jrbenito@benito.qsl.br>
2018-12-10 17:03:40 -0200

    package structure
    Signed-off-by: Josenivaldo Benito Jr <jrbenito@benito.qsl.br>

---
+++ README.md
@@ -0,0 +1,1 @@
+# Pvouput API wrapper
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pvoutput API wrapper",
+    version="0.0.1",
+    author="Josenivaldo Benito Junior",
+    author_email="jrbenito@benito.qsl.br",
+    description="Wraps API calls to pvoutput.org",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jrbenito/pvoutput",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
